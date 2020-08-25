aa7eea11c1ba2982d1d01eafb0992971b8557d3b
wyf <zimo@example.com>
2020-08-25 18:05:25 +0800

    修改Springboot测试类报错的问题 修改yml文件编码格式不对导致测试类无法正常运行的问题

--- src/test/java/com/wyf/springbootstarter/SpringBootStarterApplicationTests.java
+++ src/test/java/com/wyf/springbootstarter/SpringBootStarterApplicationTests.java
@@ -1,13 +1,20 @@
 package com.wyf.springbootstarter;
 
-import org.junit.jupiter.api.Test;
+import com.wyf.SpringBootStarterApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class SpringBootStarterApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootStarterApplication.class)
+@EnableAutoConfiguration
+public class SpringBootStarterApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void test1() {
+        System.out.println("123");
     }
 
 }
